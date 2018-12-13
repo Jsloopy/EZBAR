cfb356e3b6edab31f1fe62049df4eae219d9bb10
Jordan Sloop <jsloop310@gmail.com>
2018-12-12 16:52:30 -0800

    moved files

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './index.css';
 
 import Chart from './components/Chart/Chart.js';
 import Base from './components/Base/Base.js';
